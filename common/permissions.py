07266c0448083e5ccb404a4edecc5d8c78ddd4d9
Tristan Adams <tristangadams@gmail.com>
2020-09-24 15:07:51 -0400

    Adjusts permissions checks to use parent result in some circumstances

--- common/permissions.py
+++ common/permissions.py
@@ -13,22 +13,31 @@ class OwnedPropertyPermission(permissions.DjangoModelPermissionsOrAnonReadOnly):
         - they are either an owner or authorized editor
     """
 
-    def has_object_permission(self, request, view, obj):
+    def has_object_permission(self, request, view, obj, **kwargs):
         """ Override object permissions to check editors and owners """
+        super_result = super(OwnedPropertyPermission, self).has_object_permission(**kwargs)
         method = request.method
         user = request.user
 
-        # Ignore GET, HEAD, OPTIONS
-        if method in SAFE_METHODS:
+        # inactive users can't do anything
+        if not user.is_active:
+            return False
+
+        # admin users bypass all permissions checks
+        if user.is_superuser:
             return True
 
+        # Ignore GET, HEAD, OPTIONS for object checks
+        if method in SAFE_METHODS:
+            return super_result
+
         # Get owner and editors, if they exist.
-        # If they don't, this isn't an OwnedModel, and we should return true
+        # If they don't, this isn't an OwnedModel, and we should return the parent result
         try:
             owner = getattr(obj, 'owner')
             authorized_editors = getattr(obj, 'authorized_editors')
         except AttributeError:
-            return True
+            return super_result
 
         # Check if current user is an authorized editor
         authorized_editors_ids = []
@@ -36,6 +45,6 @@ class OwnedPropertyPermission(permissions.DjangoModelPermissionsOrAnonReadOnly):
             authorized_editors_ids = [a_e.id for a_e in authorized_editors.all()]
 
         if not owner and not authorized_editors_ids:
-            return True
+            return super_result
 
         return owner.id == user.id or user.id in authorized_editors_ids
